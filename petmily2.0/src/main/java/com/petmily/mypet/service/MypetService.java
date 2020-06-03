07753d810b657f3c76e4d3f0067ad0eda03ddfb8
seoyeong <seoyeong7.choi@gmail.com>
2020-06-03 19:50:55 +0900

    Add health

--- petmily2.0/src/main/java/com/petmily/mypet/service/MypetService.java
+++ petmily2.0/src/main/java/com/petmily/mypet/service/MypetService.java
@@ -7,6 +7,7 @@ import com.petmily.mypet.domain.PetVO;
 
 public interface MypetService {
 	public List<PetVO> petList(String memId);
+	public PetVO selectPetByNo(PetVO petVO);
 	public String insertPet(PetVO petVO);
 	public void updatePet(PetVO petVO);
 	public List<PetVO> deletePet(PetVO petVO);
